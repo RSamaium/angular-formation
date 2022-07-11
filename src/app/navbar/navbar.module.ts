8e2891ae4d4714efd0d9a7fd460c779ae0b9b071
RSamaium <sam@webcreative5.net>
2022-07-11 14:46:35 +0200

    ngfor

--- src/app/navbar/navbar.module.ts
+++ src/app/navbar/navbar.module.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { NavbarComponent } from "./navbar.component";
 import { SearchComponent } from "./search/search.component";
 
 @NgModule({
     declarations: [NavbarComponent, SearchComponent],
-    imports: [],
+    imports: [FormsModule, CommonModule],
     exports: [NavbarComponent]
 })
 export class NavbarModule {}
--- src/app/navbar/search/search.component.ts
+++ src/app/navbar/search/search.component.ts
@@ -1,12 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 
 @Component({
     selector: 'app-search',
     template: `
-        <input type="text" [value]="username">
-        <button>Rechercher</button>
+        <input type="text" [(ngModel)]="username">
+        <button (click)="search()" *ngIf="username">Rechercher</button>
+        <ul>
+            <li *ngFor="let name of firstnames">
+                {{ name }}
+            </li>
+        </ul>
     `
 })
 export class SearchComponent {
-    username: string = 'ana'
+    @Input() username: string = ''
+    @Output() onSearch: EventEmitter<string> = new EventEmitter()
+    firstnames: string[] = ['ana', 'ben', 'jim']
+
+    search() {
+        this.onSearch.emit(this.username)
+    }
 }
+
+// MVVM : Model View View-Model
+// double data binding, two way data binding
