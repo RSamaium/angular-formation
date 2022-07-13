275c6da72ff761c9f1764f48a5d7e5cce5c532eb
RSamaium <sam@webcreative5.net>
2022-07-13 15:36:29 +0200

    user edit

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -27,6 +27,10 @@ export class UserService {
         return lastValueFrom(this.http.get<User[]>(this.url))
     }
 
+    get(id: number): Observable<User> {
+        return this.http.get<User>(this.url + '/' + id)
+    }
+
     create(payload: { name: string, email: string }): Observable<User> {
         return this.http.post<User>(this.url, payload)
     }
--- src/app/layout/user-edit/user-edit.component.ts
+++ src/app/layout/user-edit/user-edit.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/core/interfaces/user';
+import { UserService } from 'src/app/core/services/user.service';
 
 @Component({
   selector: 'app-user-edit',
@@ -7,14 +10,32 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./user-edit.component.css']
 })
 export class UserEditComponent implements OnInit {
+  user: User = {} as User
+  propName: FormControl = new FormControl()
+  form: FormGroup = this.builder.group({
+    name: this.propName,
+    username: '',
+    email: ''
+  })
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private builder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((paramsUrl: any) =>{
        const id = paramsUrl.id
-       console.log(id)
+       this.userService.get(id).subscribe((user: User) => {
+         this.user = user
+         this.form.patchValue(this.user)
+       })
     })
+  }
+
+  edit() {
+
   }
 
 }
